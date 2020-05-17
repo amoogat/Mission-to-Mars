2f4e777ceb833f5d3aad3f69ddee49b9c9dca861
Aarian Moogat <amoogat@gmail.com>
2020-05-16 23:47:00 -0700

    Created app for scraping mars data

---
+++ apps/app.py
@@ -0,0 +1,27 @@
+# imports flask dependencies
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scraping
+
+app = Flask(__name__)
+
+# Setting up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# finds created mars collection and returns html template using index.html file
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+# defines flask route, accesses database, scrapes data and stores it
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return "Scraping Successful!"
+
+if __name__ == "__main__":
+   app.run()
---
+++ apps/scraping.py
@@ -0,0 +1,103 @@
+# Imports Splinter and BeautifulSoup
+from splinter import Browser
+from bs4 import BeautifulSoup
+# imports pandas
+import pandas as pd
+import datetime as dt
+
+executable_path = {'executable_path': 'chromedriver.exe'}
+
+def scrape_all():
+    # Creates path
+    browser = Browser('chrome', executable_path='chromedriver', headless=False)
+    news_title, news_paragraph = mars_news(browser)
+    # Run all scraping functions and store results in dictionary
+    data = {
+      "news_title": news_title,
+      "news_paragraph": news_paragraph,
+      "featured_image": featured_image(browser),
+      "facts": mars_facts(),
+      "last_modified": dt.datetime.now()
+    }
+    return data
+    browser.quit()
+
+# creates a function that accesses nasa website and scrapes mars data
+def mars_news(browser):
+    # Visits the nasa webiste for mars news
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+    # Delay to load page
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+
+    # sets up html parser
+    html = browser.html
+    news_soup = BeautifulSoup(html, 'html.parser')
+    #adds try/ except for handling scraping errors
+    try:
+        # searches for parent elem which desired data is stored
+        slide_elem = news_soup.select_one('ul.item_list li.slide')
+
+        # finds the first 'a' tag that is stored in parent element slide_elem
+        news_title = slide_elem.find("div", class_='content_title').get_text()
+
+        # searches for the paragraph body text of the article
+        news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+
+    except AttributeError:
+        return None, None
+        
+    return news_title, news_p
+
+# ### Featured Images
+def featured_image(browser):
+    # Visit NASA Mars URL
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
+
+    # Finds the full image button
+    full_image_elem = browser.find_by_id('full_image')
+    full_image_elem.click()
+
+    # Finds more info button and clicks
+    browser.is_element_present_by_text('more info', wait_time=1)
+    more_info_elem = browser.find_link_by_partial_text('more info')
+    more_info_elem.click()
+
+    # Parses the html with soup
+    html = browser.html
+    img_soup = BeautifulSoup(html, 'html.parser')
+    
+    # try/ except for error handling
+    try: 
+    # Finds the image url
+        img_url_rel = img_soup.select_one('figure.lede a img').get("src")
+
+    except AttributeError:
+        return None
+
+    # Uses base url to make absolute url
+    img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+
+    return img_url
+
+def mars_facts():
+    # try/ except for error handling
+    try:
+        # creqates a new dataframe of first html table encountered
+        df = pd.read_html('http://space-facts.com/mars/')[0]
+    
+    except BaseException:
+        return None
+            
+    # sets columns of description and value
+    df.columns=['Description', 'Mars']
+    # sets description as index
+    df.set_index('Description', inplace=True)
+
+    return df.to_html()
+
+
+if __name__ == "__main__":
+    # Prints scraped data if being run as a script
+    print(scrape_all())
